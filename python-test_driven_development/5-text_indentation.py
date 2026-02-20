6b4903885618ff81292f6ceb0cb32c6bc2f1f3d4
Qurban <qurbanhuseynzade2007@gmail.com>
2026-02-20 09:57:31 +0000

    Add text_indentation function with edge case tests

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This is the "5-text_indentation" module.
+It provides one function, text_indentation(text).
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+    There should be no space at the beginning or at the end of each printed line.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] in ".?:":
+            print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
