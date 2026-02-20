d7388cf490bd19aeb10304310aa73139e527eebf
Qurban <qurbanhuseynzade2007@gmail.com>
2026-02-20 10:22:40 +0000

    Fix missing argument test and final polish

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 """
-This is the "5-text_indentation" module.
-It provides one function, text_indentation(text).
+Module 5-text_indentation
 """
 
 
 def text_indentation(text):
     """
-    Prints a text with 2 new lines after each of these
-    characters: ., ? and :
-    There should be no space at the beginning or at the end
-    of each printed line.
+    Prints a text with 2 new lines after each of these: ., ? and :
     """
     if not isinstance(text, str):
-        raise TypeError("text must be a string")  # "a" əlavə edildi
+        raise TypeError("text must be a string")
 
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
+    i = 0
+    while i < len(text) and text[i] == ' ':
+        i += 1
 
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] in ".?:":
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in ".?:":
             print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
             continue
-        c += 1
+        i += 1
