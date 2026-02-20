19abc88240326254acbf7c2dcdecb62d489a0d80
Qurban <qurbanhuseynzade2007@gmail.com>
2026-02-20 09:20:33 +0000

    Handle NaN and Inf for add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """
 This is the "0-add_integer" module.
-It provides one function, add_integer(a, b).
+The module supplies one function, add_integer(a, b).
 """
 
 
 def add_integer(a, b=98):
     """
     Adds two integers or floats (casted to integers).
-    Args:
-        a: first number
-        b: second number, default 98
-    Returns:
-        Sum of a and b as an integer
+    Raises TypeError if a or b are not integers/floats, or if they are NaN/Inf.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    if a != a or abs(a) > 1.7976931348623157e+308:
+        raise TypeError("a must be an integer")
+    if b != b or abs(b) > 1.7976931348623157e+308:
+        raise TypeError("b must be an integer")
+
     return int(a) + int(b)
