35708ed0891e83faddb8efeac0f5f254bde7e527
Qurban <qurbanhuseynzade2007@gmail.com>
2026-02-22 11:33:21 +0000

    Add size to Square class

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that defines a square."""
+
+
+class Square:
+    """Class that represents a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size: The size of the new square.
+        """
+        self.__size = size
