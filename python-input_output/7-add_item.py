707468ef41d1de9889abf8442d07cbeb565d8247
Qurban <qurbanhuseynzade2007@gmail.com>
2026-03-08 11:11:18 +0000

    Fix PEP8 E722 error in 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,7 +4,6 @@ Script that adds all arguments to a Python list
 and saves them to a JSON file.
 """
 import sys
-import os
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -13,7 +12,7 @@ filename = "add_item.json"
 
 try:
     my_list = load_from_json_file(filename)
-except:
+except FileNotFoundError:
     my_list = []
 
 my_list.extend(sys.argv[1:])
