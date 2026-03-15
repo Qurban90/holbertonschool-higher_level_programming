926ba4a19bd1c9cb542b866452c4f13e11862e8f
Qurban <qurbanhuseynzade2007@gmail.com>
2026-03-15 15:11:24 +0000

    Add script to filter states containing 'a'

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,14 +12,10 @@ if __name__ == "__main__":
         ),
         pool_pre_ping=True
     )
-    
     Session = sessionmaker(bind=engine)
     session = Session()
-    
     states = session.query(State).filter(State.name.contains('a'))\
                     .order_by(State.id).all()
-    
     for state in states:
         print("{}: {}".format(state.id, state.name))
-        
     session.close()
