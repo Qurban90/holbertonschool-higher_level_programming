6b27478c5480a496e1f0bf953a9fbe70264a79bb
Qurban <qurbanhuseynzade2007@gmail.com>
2026-03-15 13:56:35 +0000

    Add script to create table with a default id value

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- Creates the table id_not_null on your MySQL server.
+-- The id column has a default value of 1.
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
